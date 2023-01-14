8cd917aecd279ebdebaeadc27935b39904681cb1
Yuri Andrianov <andrianov.yuri.spb@gmail.com>
2023-01-15 00:06:32 +0530

    Initial Commit

---
+++ TestAI/TestAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAIApp.swift
+//  TestAI
+//
+//  Created by Yuri Andrianov on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
